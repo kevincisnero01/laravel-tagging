a12117c2b9b14b18508e973a229998ac5f1d20e6
Kevin Cisnero <7884578+kevincisnero01@users.noreply.github.com>
2022-09-05 16:04:57 -0400

    Instalar laravel ui  & bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tags</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
         <h1>Sistema de Etiquetas</h1>
+
+        <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ use App\Models\Post;
 
 Route::get('/', [PostController::class, 'index'])->name('posts-index');
 Route::post('posts-save',[PostController::class, 'store'])->name('posts-store');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
