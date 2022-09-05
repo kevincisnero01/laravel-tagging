762b7ad4335e3d97b317c549b32a3ce103a58df2
Kevin Cisnero <7884578+kevincisnero01@users.noreply.github.com>
2022-09-05 17:17:33 -0400

    Vista index con bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,8 +8,31 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-        <h1>Sistema de Etiquetas</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-8 col-xs-offset-2">
+                <h1 class="page-header">Sistema de Etiquetas</h1>
+                errores
+                <br>
+                formulario
+                <hr>
+                @foreach ($posts as $post)
+                    <div class="card card-default my-2 border-primary">
+                        <div class="card-header bg-primary  text-light">{{ $post->title }}</div>
+                        <div class="card-body">{{ $post->body }}</div>
+                        <div class="card-footer border-primary">
+                            @forelse ($post->tags as $tag)
+                                <span class="rounded bg-info py-1 px-2 text-light">{{ $tag->name }}</span>
+                            @empty
+                                <em>No hay etiquetas</em>
+                            @endforelse
+                        </div>
+                    </div><!--card-->
+                @endforeach
+            </div><!--col-->
+        </div><!--row-->
+    </div><!--container-->
 
-        <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
