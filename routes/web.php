f97c309085b7ec130c1f4936d9ff463c1ae12a8a
Kevin Cisnero <7884578+kevincisnero01@users.noreply.github.com>
2022-09-05 15:05:33 -0400

    Rutas, Controlador Post  y vista index

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,8 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 use App\Models\Post;
 
-Route::get('/', function () {
-    
-    
-    $post = Post::find(1);
-    $post->tag(['php', 'laravel']);
-
-    dd($post->tags);
-});
+Route::get('/', [PostController::class, 'index'])->name('posts-index');
+Route::post('posts-save',[PostController::class, 'store'])->name('posts-store');
